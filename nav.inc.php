e6481c81eec7bb301595ab69a3b3f07b24e19387
LaurenV <laurenvdl45@gmail.com>
2022-07-12 12:00:17 +0200

    feat: complete logout and navigation

---
+++ nav.inc.php
@@ -0,0 +1,10 @@
+<div class="wrapper">
+    <ul>
+        <li>
+            <h1>Todo App</h1>
+        </li>
+        <li>
+            <a href="logout.php" class="logout"><i class="fas fa-sign-out-alt" style="color: #C78743;"></i> Afmelden</a>
+        </li>
+    </ul>
+</div>

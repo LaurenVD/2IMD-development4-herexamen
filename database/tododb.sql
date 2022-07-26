95914a6bfb4bcb7ec2a1f859961eae600794d616
LaurenV <laurenvdl45@gmail.com>
2022-07-26 15:17:20 +0200

    update: database update

--- database/tododb.sql
+++ database/tododb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Gegenereerd op: 20 jul 2022 om 13:59
+-- Gegenereerd op: 26 jul 2022 om 15:14
 -- Serverversie: 10.4.24-MariaDB
 -- PHP-versie: 7.4.29
 
@@ -39,9 +39,37 @@ CREATE TABLE `lijst` (
 --
 
 INSERT INTO `lijst` (`id`, `userId`, `title`, `description`) VALUES
-(1, 1, 'ghnbg', 'dfb'),
-(2, 1, 'nhgfcbv', 'ghfb'),
-(3, 1, 'sdxc', 'fdvxc');
+(4, 1, 'test1', 'teskt1'),
+(9, 1, 'test2', 'tekst2'),
+(10, 1, 'test3', 'tekst3'),
+(11, 1, 'test4', 'tekst4');
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `task`
+--
+
+CREATE TABLE `task` (
+  `id` int(11) NOT NULL,
+  `userId` int(11) NOT NULL,
+  `lijstId` int(11) NOT NULL,
+  `title` varchar(300) COLLATE utf8_unicode_ci NOT NULL,
+  `hour` int(11) NOT NULL,
+  `date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Gegevens worden geëxporteerd voor tabel `task`
+--
+
+INSERT INTO `task` (`id`, `userId`, `lijstId`, `title`, `hour`, `date`) VALUES
+(4, 1, 4, 'rij', 2, '2022-07-31'),
+(5, 1, 4, 'dfgser', 2, '2022-07-27'),
+(6, 0, 0, 'bvnsyrt', 3, '2022-07-28'),
+(8, 0, 0, 'syj', 1, '2022-07-13'),
+(9, 0, 0, 'sgfh', 2, '2022-06-30'),
+(19, 1, 0, 'test', 2, '2022-07-26');
 
 -- --------------------------------------------------------
 
@@ -70,6 +98,13 @@ INSERT INTO `users` (`id`, `username`, `password`) VALUES
 -- Indexen voor tabel `lijst`
 --
 ALTER TABLE `lijst`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `userId` (`userId`);
+
+--
+-- Indexen voor tabel `task`
+--
+ALTER TABLE `task`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -86,16 +121,31 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT voor een tabel `lijst`
 --
 ALTER TABLE `lijst`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT voor een tabel `task`
+--
+ALTER TABLE `task`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 
 --
 -- AUTO_INCREMENT voor een tabel `users`
 --
 ALTER TABLE `users`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- Beperkingen voor geëxporteerde tabellen
+--
+
+--
+-- Beperkingen voor tabel `lijst`
+--
+ALTER TABLE `lijst`
+  ADD CONSTRAINT `lijst_ibfk_1` FOREIGN KEY (`userId`) REFERENCES `users` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-

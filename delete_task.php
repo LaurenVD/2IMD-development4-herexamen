53a12b8268407568f224c9f9c13802cd9af35743
LaurenV <laurenvdl45@gmail.com>
2022-08-15 10:04:32 +0200

    feat: tasks can be removed if user is the creator of the list

--- delete_task.php
+++ delete_task.php
@@ -1,6 +1,15 @@
 <?php
     include_once('core/autoload.php');
+
+    $listId = $_GET["listId"];
+    $list = TodoList::getListArrayById($listId);
+
+    if($list["userId"] != $_SESSION["userId"]){
+        header("Location: index.php");
+        die;
+     }
     
     Task::deleteTask($_GET["task"]);
     header("Location: index.php");
+    die;
 ?>

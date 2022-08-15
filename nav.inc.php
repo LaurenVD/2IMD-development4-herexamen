a3ae843ca3fddb3ac37b073eaa0480c04b803421
LaurenV <laurenvdl45@gmail.com>
2022-08-15 10:01:47 +0200

    feat: admin page is shown in the navigation for admins

--- nav.inc.php
+++ nav.inc.php
@@ -2,5 +2,8 @@
 
 <div class="container">
     <h1>TODO APP</h1>
+    <?php if(isset($_SESSION['is_admin']) && $_SESSION['is_admin'] === true): ?>
+    <a href="admin.php">Admin</a>
+    <?php endif; ?>
     <a href="logout.php" class="logout">Afmelden</a>
 </div>
